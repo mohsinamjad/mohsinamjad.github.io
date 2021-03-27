64449ea2a1e0368fe7237ae9c34ea8bd04e4783e
Mohsin <mohsinamjad51@gmail.com>
2021-03-27 17:04:34 +0500

    Updates

---
+++ _nuxt/static/1616846659/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Ftempindex","\u002Fblogs","\u002Fblogs\u002F8a7ad8c6-9837-4d6d-8c59-3a88d44c0ec4"]})
---
+++ _nuxt/static/1616846659/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616846659",layout:"default",error:a,state:{appBarColor:b,appTitleColor:b},serverRendered:true,routePath:c,config:{app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},apollo:{defaultClient:Object.create(null)}}}(null,"#FFF","\u002F"));
